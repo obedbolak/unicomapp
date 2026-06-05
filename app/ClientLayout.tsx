9538ff616efc527630e22a3a1354c348c3af0a80
obedbolak <jamesnormankate@gmail.com>
2026-06-05 07:47:07 +0100

    updates

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Scene3DOptimized from "@/components/Scene3DOptimized";
 
 export default function ClientLayout({
@@ -8,15 +8,24 @@ export default function ClientLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
   return (
     <>
-      {/* 3D Scene Background - Fixed behind everything */}
-      <div className="fixed inset-0 w-screen h-screen -z-10 pointer-events-none">
-        <Scene3DOptimized />
+      {/* 3D Scene Background */}
+      <div style={{
+        position: "fixed", top: 0, left: 0,
+        width: "100%", height: "100%",
+        zIndex: 0, pointerEvents: "none",
+      }}>
+        {mounted && <Scene3DOptimized />}
       </div>
 
-      {/* Page Content - Scrolls over the fixed background */}
-      <div className="relative z-10 w-full">{children}</div>
+      {/* Page Content */}
+      <div style={{ position: "relative", zIndex: 1, width: "100%", minHeight: "100vh" }}>
+        {children}
+      </div>
     </>
   );
 }
